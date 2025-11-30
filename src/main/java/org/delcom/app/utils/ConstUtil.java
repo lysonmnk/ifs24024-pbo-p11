55ac19a8046829a8349f050b473b6a2034bbaa34
lysonmnk <lysonmanik30@gmail.com>
2025-11-30 21:15:40 +0700

    new

--- src/main/java/org/delcom/app/utils/ConstUtil.java
+++ src/main/java/org/delcom/app/utils/ConstUtil.java
@@ -1,11 +1,22 @@
 package org.delcom.app.utils;
 
 public class ConstUtil {
-    public static final String KEY_AUTH_TOKEN = "AUTH_TOKEN";
-    public static final String KEY_USER_ID = "USER_ID";
-
+    // Home (Landing Page) 
+    public static final String TEMPLATE_PAGES_HOME = "pages/home";
+    
+    // Template paths untuk Todo
+    public static final String TEMPLATE_PAGES_TODOS_HOME = "pages/todos/home";
+    public static final String TEMPLATE_PAGES_TODOS_DETAIL = "pages/todos/detail";
+    
+    // Template paths untuk CashFlow
+    public static final String TEMPLATE_PAGES_CASHFLOWS_HOME = "pages/cashflows/home";
+    public static final String TEMPLATE_PAGES_CASHFLOWS_DETAIL = "pages/cashflows/detail";
+    
+    // Auth templates
     public static final String TEMPLATE_PAGES_AUTH_LOGIN = "pages/auth/login";
     public static final String TEMPLATE_PAGES_AUTH_REGISTER = "pages/auth/register";
-    public static final String TEMPLATE_PAGES_HOME = "pages/home";
-    public static final String TEMPLATE_PAGES_TODOS_DETAIL = "pages/todos/detail";
+
+    public static final String TEMPLATE_PAGES_MENU = "pages/menu";
+
+    
 }
